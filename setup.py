c382ec6f4470d43ae183904aa966144371f32ae9
Pierluigi Vinciguerra <pierluigivinciguerra@Targarian.homenet.telecomitalia.it>
2022-05-04 18:30:09 +0200

    Minor updates

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ import sys
 from setuptools import setup
 
 setup(	name='advanced-scrapy-proxies',
-		version='0.1',
+		version='0.1.1',
 		description='Advanced Scrapy Proxies: random proxy middleware for Scrapy with advanced features',
+		long_description=README,
+		long_description_content_type="text/markdown",
 		author='Pierluigi Vinciguerra',
 		author_email='pierluigivinciguerra@gmail.com',
+		license="GNU GPLv2",
 		url='https://github.com/PVinciguerra/advanced-scrapy-proxies',
 		packages=['advanced-scrapy-proxies'],
-		setup_requires=['wheel']
+		setup_requires=['wheel', 'requests', 'pip']
 		)
